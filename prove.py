d280793d9c783334dfe786ba34fd7e712981b49e
Daniele Tarek Iaisy <danieletarek.iaisy@studio.unibo.it>
2021-10-20 18:22:31 +0000

    Replace prove.py

--- prove.py
+++ prove.py
@@ -12,12 +12,29 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 #oggetto API per interazione con twitter
 api = tweepy.API(auth)
-print(type(api))
-public_tweets: tweepy.models.ResultSet = api.home_timeline()
-print(type(public_tweets))
+#public_tweets: tweepy.models.ResultSet = api.home_timeline()
+
+#tweet_mode = extended così da ottenere i tweet senza testo troncato
+public_tweets = api.search_tweets('Roma', tweet_mode='extended')
+#public_tweets = tweepy.Cursor(api.search, q='Roma').items(15)
+tot = 0
+done = False
 for tweet in public_tweets:
-    #print(type(tweet))
-    """print('////////////////////////////////////////')
-    print(tweet.user.screen_name)
-    print(tweet.text)
-    print('////////////////////////////////////////', end='\n\n')"""
+    print('////////////////////////////////////////')
+    print('url: ', tweet.user.screen_name, '/status/', tweet.id, sep='')
+    #quando fai la richiesta con tweet_mode=extended il testo sta nel campo full_text
+    print(tweet.full_text)
+    #se è un retweet (<=> il testo inizia con RT) è definito la proprietà retweeted_status
+    #e il testo del tweet originale sta dentro a full_text (credo debba essere text se non
+    #si usa tweet_mode='extended' nella richiesta)
+    try:
+        print(tweet.retweeted_status.full_text)
+    #se non è un retweet retweeted_status non è definito e si entra qui dentro
+    except Exception as e:
+        print(e)
+    print('////////////////////////////////////////')
+    print('\n\n')
+
+    tot += 1
+
+print('trovati', tot, 'tweets')
