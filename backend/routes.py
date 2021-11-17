ba8cfb9f7ea38283fee6ca90b5893e175bcf92da
Daniele Tarek Iaisy <danieletarek.iaisy@studio.unibo.it>
2021-11-17 22:02:33 +0000

    Replace routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,32 +1,43 @@
 import flask as fl
+import os
+
 import twitter_client as tc
 
 FRONT_DIR = '../frontend/'
-server = fl.Flask(__name__, template_folder=FRONT_DIR)
+CONTENT_MODE = 'content'
+USER_MODE = 'user'
+BUILD_DIR = '../frontend/build/'
+server = fl.Flask(__name__, template_folder=FRONT_DIR, static_folder=BUILD_DIR)
 
 
-@server.route('/', methods=['GET'])
-def home():
-    return fl.render_template("main_page.html")
-
-
-@server.route('/styles/<string:fileName>', methods=['GET'])
-def style(fileName: str):
-    try:
-        return fl.send_from_directory(FRONT_DIR, fileName, as_attachment=True)
-    except FileNotFoundError:
-        fl.abort(404)
+@server.route('/', defaults={'path': ''})
+@server.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(server.static_folder + '/' + path):
+        return fl.send_from_directory(server.static_folder, path)
+    else:
+        return fl.send_from_directory(server.static_folder, 'index.html')
 
 
 @server.route('/tweets', methods=['GET'])
 def tweets():
-    headers = {"Content-Type": "application/json"}
-    content = tc.search_by_content('')
-    return make_response(
-        'Tweets found',
-        200,
-        content
-    )
+    """ Endpoint di API per la richiesta dei tweet da parte dei clients.
+        Il parametro query mode specifica che tipo di ricerca va fatta (tra
+        content, user).
+        Il parametro query by specifica il contenuto sulla base del quale
+        va fatta la richiesta"""
+    mode = fl.request.args.get('mode')
+    query = fl.request.args.get('by')
+    amount = fl.request.args.get('amount')
+    if amount == None or amount == '':
+        amount = tc.MAX_TWEETS
+
+    if mode == CONTENT_MODE:
+        content = tc.search_by_content(query, amount)
+    elif mode == USER_MODE:
+        content = tc.search_by_username(query, amount)
+
+    return fl.jsonify(content)
 
 
 @server.errorhandler(404)
