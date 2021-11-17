d7ffc8d8471bbd317fbf86e2b932375135242eb7
Daniele Tarek Iaisy <danieletarek.iaisy@studio.unibo.it>
2021-11-17 22:02:48 +0000

    Replace twitter_client.py

--- backend/twitter_client.py
+++ backend/twitter_client.py
@@ -5,9 +5,56 @@ import keys
 MAX_TWEETS = 100
 
 
-def jsonify(tweets):
+def is_retweet(tweet) -> bool:
+    """ Ritorna True tweet è lo status di un retweet,
+        False altrimenti"""
+    try:
+        #prova a sollevare un'eccezione accedendo a campi definiti
+        #sse si tratta di un retweet
+        tweet.retweeted_status.full_text
+        print('ATTENZIONE CAZZOOOOO QUA CE UN RETWEET')
+        return True
+    #se non è un retweet retweeted_status non è definito e si entra qui dentro
+    except AttributeError:
+        return False
+
+
+def dictify_user(user) -> dict:
+    """ Ritorna una rappresentazione in forma di dizionario di python
+        del record di user ottenuto dallo status di un tweet"""
+    user_dict = { }
+    user_dict['screen_name'] = user.screen_name
+    return user_dict
+
+
+def dictify_single_tweet(tweet) -> dict:
+    """ Ritorna una rappresentazione in forma di dizionario di python
+        dello status di tweet"""
+    tweet_dict = { }
+    tweet_dict['user'] = dictify_user(tweet.user)
+    if is_retweet(tweet):
+        tweet_dict['retweeted_status'] = dictify_single_tweet(tweet.retweeted_status)
+    else:
+        tweet_dict['full_text'] = tweet.full_text
+
+    return tweet_dict
+
+
+def dictify_tweets(tweets) -> dict[dict]:
+    """ Ritorna una rappresentazione in forma di dizionario di python
+        di tweets. Il dizionario è indicizzato su interi da 0 a n come
+        fosse una lista"""
+    #tweets_list = {}
+    tweets_list = []
+    i = 0
+
     for tweet in tweets:
-        pass
+        tweet_dict = dictify_single_tweet(tweet)
+        tweets_list.append(tweet_dict)
+        #tweets_list[i] = tweet_dict
+        i += 1
+
+    return tweets_list
 
 
 def printTweet(tweet):
@@ -31,33 +78,44 @@ auth.set_access_token(keys.access_token(), keys.access_token_secret())
 api = tweepy.API(auth)
 #public_tweets: tweepy.models.ResultSet = api.home_timeline()
 
+def inutile():
+    #while True:
+    #    print('Inserisci una stringa da cercare (% per uscire)')
+    #    keyword = input()
+    #    if keyword == '%':
+    #        break
+    #    #questo evita di ritornare tweet che contengono la keyword nello username del twittatore
+    #    keyword += f'-from:{keyword}'
+    #    #tweet_mode = extended così da ottenere i tweet senza testo troncato
+    #    public_tweets = api.search_tweets(keyword, tweet_mode='extended', count=100)
+    #    for tweet in public_tweets:
+    #        if keyword in tweet.user.screen_name:
+    #            print(f'LA KEYWORD STA NELLO USERNAME PD [{tweet.user.screen_name}]')
+    #            printTweet(tweet)
+    #            break
+    #        else:
+    #            printTweet(tweet)
+    #    print(f'scaricati in tutto {len(public_tweets)} tweet')
+    pass
 
-while True:
-    print('Inserisci una stringa da cercare (% per uscire)')
-    keyword = input()
-    if keyword == '%':
-        break
-    #questo evita di ritornare tweet che contengono la keyword nello username del twittatore
-    keyword += f'-from:{keyword}'
-    #tweet_mode = extended così da ottenere i tweet senza testo troncato
-    public_tweets = api.search_tweets(keyword, tweet_mode='extended', count=100)
-    for tweet in public_tweets:
-        if keyword in tweet.user.screen_name:
-            print(f'LA KEYWORD STA NELLO USERNAME PD [{tweet.user.screen_name}]')
-            printTweet(tweet)
-            break
-        else:
-            printTweet(tweet)
-    print(f'scaricati in tutto {len(public_tweets)} tweet')
 
-
-def search_by_content(content: str, amount: int=MAX_TWEETS):
-    query = content + f'-from:{content}'
+def search_by_content(content: str, amount: int = MAX_TWEETS) -> dict[dict]:
+    """ Interroga l'API di twitter per la ricerca di tweet che nel loro testo
+        includono content. Il numero di tweet richiesti è specificato da amount
+        settato di default a MAX_TWEETS, il massimo; qualsiasi quantitativo
+        superiore sarà limitato a MAX_TWEETS.
+        I tweets sono ritornati in una lista sottoforma di dizionario"""
+    query = content + f'-from:{content} -filter:retweets'
     found_tweets = api.search_tweets(query, tweet_mode='extended', count=amount)
-    return found_tweets
+    return dictify_tweets(found_tweets)
 
 
-def search_by_username(username: str, amount: int=MAX_TWEETS):
-    query = f'from:{username}'
+def search_by_username(username: str, amount: int = MAX_TWEETS) -> dict[dict]:
+    """ Interroga l'API di twitter per la ricerca di tweet postati dall'account
+        username. Il numero di tweet richiesti è specificato da amount
+        settato di default a MAX_TWEETS, il massimo; qualsiasi quantitativo
+        superiore sarà limitato a MAX_TWEETS.
+        I tweets sono ritornati in una lista sottoforma di dizionario"""
+    query = f'from:{username} -filter:retweets'
     found_tweets = api.search_tweets(query, tweet_mode='extended', count=amount)
-    return found_tweets
+    return dictify_tweets(found_tweets)
