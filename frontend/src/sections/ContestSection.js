2e54b564cef1b9e4b289dd687f74fc89b0353ba3
Daniele Tarek Iaisy <danieletarek.iaisy@studio.unibo.it>
2021-12-18 22:11:15 +0000

    Upload New File

---
+++ frontend/src/sections/ContestSection.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+
+
+
+class ContestSection extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>contest</div>
+    );
+  }
+}
+
+
+export default ContestSection;
