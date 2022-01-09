a24ef184b2d9cf520ab51ecc2d4bc555f7815bc0
Daniele Tarek Iaisy <danieletarek.iaisy@studio.unibo.it>
2022-01-09 22:46:18 +0000

    Replace contest.py

--- backend/contest.py
+++ backend/contest.py
@@ -26,13 +26,15 @@ class Tale:
         d['id'] = self.id
         d['creator'] = self.creator
         d['text'] = self.text
+        return d
 
 
 class Contest:
-    def __init__(self, id, tales: list[str], organizer: str):
+    def __init__(self, id, tales: list[str], organizer: str, name: str):
         self._id = id
         self._tales: list[str] = tales
         self._organizer: str = organizer
+        self._name: str = name
 
     @property
     def id(self):
@@ -46,6 +48,10 @@ class Contest:
     def organizer(self) -> str:
         return self._organizer
 
+    @property
+    def name(self):
+        return self._name
+
     def add_tale(self, tale: Tale):
         self._tales.append(tale)
 
@@ -53,18 +59,22 @@ class Contest:
         d = {}
         d['tales'] = self.tales
         d['id'] = self.id
+        d['organizer'] = self.organizer
+        d['name'] = self.name
+        return d
 
 
 next_contest_id: int = 0
 next_tale_id: int = 0
 initialized: bool = False
-contests: dict[str, Contest] = {}
-tales: dict[str, Tale] = {}
+contests: dict[int, Contest] = {}
+tales: dict[int, Tale] = {}
 
 
 def module_init():
-    if not initialized:
+    global initialized
 
+    if not initialized:
 
         initialized = True
 
@@ -77,29 +87,39 @@ def get_contest_list() -> list[dict]:
     return l
 
 
+def get_tale_list() -> list[dict]:
+    t: list[dict] = []
+    for tale in tales.values():
+        t.append(tale.toDict())
+
+    return t
+
+
 def save_contests():
     contest_list: list[dict] = get_contest_list()
-    dbmanager.save_contests()
+    dbmanager.save_contests(contest_list)
+
 
 def save_tales():
     tale_list: list[dict] = get_tale_list()
-    dbmanager.save_tales()
+    dbmanager.save_tales(tale_list)
 
 
-def register_contest(organizer: str):
-    #istanziare un nuovo contest
-    c: Contest = Contest(next_contest_id, [], organizer)
-    #metterlo nel dizionario
+def register_contest(organizer: str, name: str) -> Contest:
+    global next_contest_id
+
+    c: Contest = Contest(next_contest_id, [], organizer, name)
     contests[c.id] = c
-    #aggiornare l'id
     next_contest_id += 1
     #memorizzarlo su disco
-    save_contests()
+    #save_contests()
 
     return c
 
 
 def register_tale(creator: str, text: str):
+    global next_tale_id
+
     t: Tale = Tale(next_tale_id, creator, text)
     tales[t.id] = t
     next_tale_id += 1
@@ -110,10 +130,16 @@ def register_tale(creator: str, text: str):
 
 def add_tale_to(tale_id: str, contest_id: str):
     c: Contest = contests[contest_id]
-    Contest.append(tale_id)
+    c.append(tale_id)
     save_contests()
 
 
+def is_tale_registered(tale_id: int) -> bool:
+    return tale_id in tales.values()
+
+
+def is_contest_registered(contest_id: int) -> bool:
+    return contest_id in contests.values()
 
 
 module_init()
