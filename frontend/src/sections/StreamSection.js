a1b3be2f19b0826b657715b5ce946808b0d323f3
Andrea Cristiano <andrea.cristiano@studio.unibo.it>
2022-03-02 17:20:58 +0000

    Replace StreamSection.js

--- frontend/src/sections/StreamSection.js
+++ frontend/src/sections/StreamSection.js
@@ -8,6 +8,11 @@ import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TweetList from '../components/TweetList';
+import Divider from '@mui/material/Divider';
+import Tweet from '../components/Tweet';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Grid from '@mui/material/Grid';
 
 
 
@@ -20,87 +25,82 @@ class StreamSection extends Component{
     constructor(props) {
         super(props)
         this.state={
-            tweetsLoaded:false,
+            tweetsLoaded: 0
         }
-
-    this.tweets=[];
-
+        this.tweets=[]
 
     }
 
-    fetchData = async ()=>{
-            var index = window.location.href.indexOf('?');
-            console.log("eheh..")
-            //var queryString = window.location.href.substr(index);
-            var queryString= "?by=roma"
-            await fetch("http://127.0.0.1:5000/stream"+ queryString,{
-            method: 'GET',
-            headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin' : '*',
-            'Accept': 'application/json'
-            }
-            })
-            .then(response =>{
-              console.log("diobo");
-              response.json()
-              //console.log(response.json())
-            } )
-            .then(data => {
-                console.log("chissà....")
-                const ao=data
-                console.log(ao);
-                this.tweets.push(data)
-            })
-            .catch(e => {
-                console.log("diocane.....")
-                console.log(e);
-            });
-        }
 
-        getjson= async ()=>{
+
+        getjson(){
+            var count=0
             var queryString= "?by=roma"
-            await $.getJSON("http://127.0.0.1:5000/stream"+ queryString, (data)=>{
-                console.log("daje")
-                console.log(data)
-                this.tweets.push(data)
-            } )
+            $.get("http://127.0.0.1:5000/data" , (data)=>{
+                count++;
+                  //console.log("IN ARRIVO!!!!!!!")
+                  console.log(data)
+                  data.map(tweet=>{
+                    this.tweets.push(tweet)
+                  })
+                  this.setState({tweetsLoaded: count})
+                  
+              }).catch(e => {
+                  console.log("diocane.....")
+                  console.log(e);
+              });
 
         }
 
 
-         async componentDidMount(){
+          componentDidMount(){
 
-            await this.fetchData();
-            //await this.getjson()
-
-            await setInterval(()=>{
-                //this.getjson();
-                this.fetchData();
-            }, 5000*2);
-
+            console.log("pippos")
             
-        }
+            $.get("http://127.0.0.1:5000/stream")
+            this.getjson()
+
+            setInterval(()=>{
+
+              this.getjson()
+
+            }, 5000);
+            
+           }
+
 
     render(){
 
+
+
         return(
-            <div>
+            <div className="robaccia">
+                <Divider/>
+                
 
-                <form method="GET" action="/stream">
+                <List>
 
-                    <TextField name="by" label="Standard" variant="standard" />
+                {this.tweets.map((tweet, index) => {
+                  return (
+                    <div>
+                      
+                      <ListItem key={index}>
+                        <Tweet content={tweet}>
+                        </Tweet>
+
+                      </ListItem>
+                    <Divider/>
+                    </div>
+                  );
+                })}
 
 
-                    <Button>Search</Button>
-                    <Button>Stop</Button>
-
-                </form>
 
 
+        </List>
 
             </div>
-        );
+        )
 
     }
 
@@ -114,6 +114,17 @@ export default StreamSection
 
 
 /*
+<form method="GET" action="/stream">
+
+    <TextField name="by" label="Standard" variant="standard" />
+
+
+    <Button>Search</Button>
+    <Button>Stop</Button>
+
+</form>
+
+
 var index = window.location.href.indexOf('?');
         if (index !== -1){
             var queryString = window.location.href.substr(index);
@@ -135,7 +146,19 @@ var index = window.location.href.indexOf('?');
         }
 
 
+        {this.tweets.map((tweet, index) => {
+          return (
+            <div>
 
+              <ListItem key={index}>
+                <Tweet content={tweet}>
+                </Tweet>
+
+              </ListItem>
+            <Divider/>
+            </div>
+          );
+        })}
 
 
 */
