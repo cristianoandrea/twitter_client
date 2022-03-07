96e3cb3a67b9cf66444a33de11a8c1c76c4b670f
Andrea Cristiano <andrea.cristiano@studio.unibo.it>
2022-03-07 10:33:24 +0000

    Replace routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,18 +1,35 @@
 import flask as fl
+from flask import jsonify
 from flask_cors import CORS
 import os
-
+import stream as s
+from info import data
 import twitter_client as tc
 import trivia
+import contest
+import threading
+import sentiment as sa
 
 FRONT_DIR = '../frontend/'
 CONTENT_MODE = 'content'
 USER_MODE = 'user'
+PLACE_MODE = 'place'
 BUILD_DIR = '../frontend/build/'
 server = fl.Flask(__name__, template_folder=FRONT_DIR, static_folder=BUILD_DIR)
 #serve per eliminare i problemi di richieste a porte diverse in fase
 #di testing
 CORS(server)
+server.config['CORS_HEADERS'] = 'Content-Type'
+prev_list = list()
+
+
+class Thread (threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print("overwatch2")
+        s.use_stream()
 
 
 @server.route('/', defaults={'path': ''})
@@ -42,10 +59,55 @@ def tweets():
         pass
     if mode == CONTENT_MODE:
         content = tc.search_by_content(query, amount)
+        sentiment_list = sa.final(content)
+        for t, b in zip(content, sentiment_list):
+            t['sentiment'] = b['sentiment']
+        print(content)
     elif mode == USER_MODE:
         content = tc.search_by_username(query, amount)
+        sentiment_list = sa.final(content)
+        for t, b in zip(content, sentiment_list):
+            t['sentiment'] = b['sentiment']
+    elif mode == PLACE_MODE:
+        content = tc.search_by_geotag(query, amount)
+        sentiment_list = sa.final(content)
+        for t, b in zip(content, sentiment_list):
+            t['sentiment'] = b['sentiment']
+        
 
     return fl.jsonify(content)
+
+
+@server.route('/stream', methods=['GET'])
+def stream():
+    t = Thread()
+    ##query = fl.request.args.get('by')
+    t.run()
+    return
+
+
+@server.route('/data', methods=['GET'])
+def stream_data():
+    try:
+        new_data = []
+        for tweet in data:
+            new_data.append(tweet)
+
+        global prev_list
+        if prev_list == []:
+            print("lello scopa i cani")
+            prev_list = new_data[:]
+            return fl.jsonify(new_data)
+        else:
+            d = []
+            for item in new_data:
+                if item not in prev_list:
+                    print("lello è gay")
+                    d.append(item)
+            prev_list = new_data[:]
+            return fl.jsonify(d)
+    except Exception:
+        pass
 
 
 @server.route('/question', methods=['POST'])
@@ -84,6 +146,51 @@ def answers_of():
     username = fl.request.args.get('username')
     answers = tc.search_by_username_with_content(username, trivia.ANSWER_TAG)
     return fl.jsonify(answers)
+
+
+@server.route('/contests', methods=['POST'])
+def post_contest():
+    organizer = fl.request.form.get('organizer')
+    name = fl.request.form.get('name')
+    new_contest = contest.register_contest(organizer, name)
+    return new_contest.toDict()
+
+
+@server.route('/contests', methods=['GET'])
+def get_contests():
+    return fl.jsonify(contest.get_contest_list(True))
+
+
+@server.route('/contests/votes', methods=['GET'])
+def get_votes_of():
+    contest_id: str = fl.request.args.get('contest')
+    tweets = tc.search_votes(contest_id)
+    obj = contest.make_vote_object(int(contest_id), tweets)
+    print(obj)
+    return obj
+
+
+@server.route('/tales', methods=['POST'])
+def post_tale():
+    creator: str = fl.request.form.get('creator')
+    text: str = fl.request.form.get('text')
+    new_tale: str = contest.register_tale(creator, text)
+    return new_tale.toDict()
+
+
+@server.route('/contests/tales', methods=['POST'])
+def add_tale_to_contest():
+    tale_id: int = int(fl.request.form.get('tale'))
+    contest_id: int = int(fl.request.form.get('contest'))
+    retVal = None
+    #controllare che gli id siano registrati
+    if contest.is_tale_registered(tale_id) and contest.is_contest_registered(contest_id):
+        contest.add_tale_to(tale_id, contest_id)
+        retVal = True
+    else:
+        retVal = False
+
+    return fl.jsonify(retVal)
 
 
 @server.errorhandler(404)
