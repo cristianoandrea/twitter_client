96e6fba8333271a09b409b07195dbfcd2fff8221
Andrea Cristiano <andrea.cristiano@studio.unibo.it>
2022-03-07 10:39:27 +0000

    Replace twitter_client.py

--- backend/twitter_client.py
+++ backend/twitter_client.py
@@ -1,25 +1,25 @@
 import tweepy
-
+import requests
 import keys
+from random import randrange
 
 MAX_TWEETS = 100
 
 
-def is_retweet(tweet) -> bool:
-    """ Ritorna True tweet è lo status di un retweet,
-        False altrimenti"""
+def is_retweet(tweet):
+
     try:
         #prova a sollevare un'eccezione accedendo a campi definiti
         #sse si tratta di un retweet
         tweet.retweeted_status.full_text
         print('ATTENZIONE CAZZOOOOO QUA CE UN RETWEET')
         return True
-    #se non è un retweet retweeted_status non è definito e si entra qui dentro
+
     except AttributeError:
         return False
 
 
-def text_from_dict(tweet_dict: dict) -> str:
+def text_from_dict(tweet_dict):
     """ Ritorna il testo contenuto del tweet sotto forma di dizionario, così
         come è creato dalla funzione dictify_single_tweet. Nel caso il tweet
         dovesse essere quello di un retweet ritorna il testo del tweet
@@ -30,7 +30,26 @@ def text_from_dict(tweet_dict: dict) -> str:
         return tweet_dict['full_text']
 
 
-def dictify_user(user) -> dict:
+def dictify_place(place: str):
+    if place != None:
+        place_dict = {}
+        x = place.partition(",")
+        posti_lat = float(x[0])
+        posti_long = float(x[2])
+        print(posti_lat)
+        print(posti_long)
+        diff_lat = randrange(-50, 50)
+        diff_long = randrange(-50, 50)
+        posti_lat = posti_lat+(diff_lat/1000)
+        posti_long = posti_long+(diff_long/1000)
+        print(posti_lat)
+        print(posti_long)
+        place_dict['lat'] = posti_lat
+        place_dict['long'] = posti_long
+        return place_dict
+
+
+def dictify_user(user):
     """ Ritorna una rappresentazione in forma di dizionario di python
         del record di user ottenuto dallo status di un tweet"""
     user_dict = {}
@@ -39,11 +58,17 @@ def dictify_user(user) -> dict:
     return user_dict
 
 
-def dictify_single_tweet(tweet) -> dict:
+def dictify_single_tweet(tweet, place):
     """ Ritorna una rappresentazione in forma di dizionario di python
         dello status di tweet"""
+
     tweet_dict = {}
     tweet_dict['user'] = dictify_user(tweet.user)
+    if place == "a":
+        tweet_dict['place'] = ""
+    else:
+        tweet_dict['place'] = dictify_place(place)
+
     if is_retweet(tweet):
         tweet_dict['retweeted_status'] = dictify_single_tweet(
             tweet.retweeted_status)
@@ -53,31 +78,49 @@ def dictify_single_tweet(tweet) -> dict:
     return tweet_dict
 
 
-def listify_tweets(tweets) -> list[dict]:
+def listify_tweets(tweets, place):
     """ Ritorna una rappresentazione in forma di dizionario di python
         di tweets. Il dizionario è indicizzato su interi da 0 a n come
         fosse una lista"""
     tweets_list = []
 
     for tweet in tweets:
-        tweet_dict = dictify_single_tweet(tweet)
+        tweet_dict = dictify_single_tweet(tweet, place)
         tweets_list.append(tweet_dict)
 
     return tweets_list
 
 
 def printTweet(tweet):
-    print('////////////////////////////////////////')
-    print(f'@{tweet.user.screen_name}')
-    #quando fai la richiesta con tweet_mode=extended il testo sta nel campo full_text
-    try:
-        print(f'è un retweet di @{tweet.retweeted_status.user.screen_name}')
-        print(tweet.retweeted_status.full_text)
-    #se non è un retweet retweeted_status non è definito e si entra qui dentro
-    except AttributeError:
-        print(tweet.full_text)
-    print('////////////////////////////////////////')
-    print(end='\n\n')
+    #print('////////////////////////////////////////')
+    if tweet.place:
+
+        """#print("Place:")
+        print(tweet.place.bounding_box.coordinates[0][1])
+        posti_long1=(tweet.place.bounding_box.coordinates[0][0][0] + tweet.place.bounding_box.coordinates[0][1][0])/2
+        posti_lat1=(tweet.place.bounding_box.coordinates[0][0][1]+tweet.place.bounding_box.coordinates[0][1][1])/2
+        posti_long2=(tweet.place.bounding_box.coordinates[0][2][0]+tweet.place.bounding_box.coordinates[0][3][0])/2
+        posti_lat2=(tweet.place.bounding_box.coordinates[0][2][1]+tweet.place.bounding_box.coordinates[0][3][1])/2
+        posti_long=(posti_long1+posti_long2)/2
+        posti_lat=(posti_lat1+posti_lat2)/2
+        print(posti_long)
+        print(posti_lat)"""
+
+        #print(tweet.place.bounding_box.coordinates[0][0])
+        print(f'@{tweet.user.screen_name}')
+        #quando fai la richiesta con tweet_mode=extended il testo sta nel campo full_text
+        try:
+
+            print("------")
+            print(
+                f'è un retweet di @{tweet.retweeted_status.user.screen_name}')
+            print(tweet.retweeted_status.full_text)
+
+        #se non è un retweet retweeted_status non è definito e si entra qui dentro
+        except AttributeError:
+            print(tweet.full_text)
+        print('////////////////////////////////////////')
+        print(end='\n\n')
 
 
 #autorizzazione via tweepy con informazioni di accesso
@@ -88,37 +131,16 @@ api = tweepy.API(auth)
 #public_tweets: tweepy.models.ResultSet = api.home_timeline()
 
 
-def inutile():
-    #while True:
-    #    print('Inserisci una stringa da cercare (% per uscire)')
-    #    keyword = input()
-    #    if keyword == '%':
-    #        break
-    #    #questo evita di ritornare tweet che contengono la keyword nello username del twittatore
-    #    keyword += f'-from:{keyword}'
-    #    #tweet_mode = extended così da ottenere i tweet senza testo troncato
-    #    public_tweets = api.search_tweets(keyword, tweet_mode='extended', count=100)
-    #    for tweet in public_tweets:
-    #        if keyword in tweet.user.screen_name:
-    #            print(f'LA KEYWORD STA NELLO USERNAME PD [{tweet.user.screen_name}]')
-    #            printTweet(tweet)
-    #            break
-    #        else:
-    #            printTweet(tweet)
-    #    print(f'scaricati in tutto {len(public_tweets)} tweet')
-    pass
-
-
-def make_query(query: str, amount: int = MAX_TWEETS) -> list[dict]:
+def make_query(query: str, amount: int = MAX_TWEETS):
     """ Helper function che effettua effettivamente la richiesta ai server
         di twitter e ritorna una lista di dizionari che rappresentano gli
         oggetti"""
     found_tweets = api.search_tweets(
         query, tweet_mode='extended', count=amount)
-    return listify_tweets(found_tweets)
+    return listify_tweets(found_tweets, "a")
 
 
-def search_by_content(content: str, amount: int = MAX_TWEETS, remove_user: bool = True) -> list[dict]:
+def search_by_content(content: str, amount: int = MAX_TWEETS, remove_user: bool = True):
     """ Interroga l'API di twitter per la ricerca di tweet che nel loro testo
         includono content. Il numero di tweet richiesti è specificato da amount
         settato di default a MAX_TWEETS, il massimo; qualsiasi quantitativo
@@ -132,7 +154,7 @@ def search_by_content(content: str, amount: int = MAX_TWEETS, remove_user: bool 
     return make_query(query, amount)
 
 
-def search_by_username(username: str, amount: int = MAX_TWEETS) -> list[dict]:
+def search_by_username(username: str, amount: int = MAX_TWEETS):
     """ Interroga l'API di twitter per la ricerca di tweet postati dall'account
         username. Il numero di tweet richiesti è specificato da amount
         settato di default a MAX_TWEETS, il massimo; qualsiasi quantitativo
@@ -142,10 +164,65 @@ def search_by_username(username: str, amount: int = MAX_TWEETS) -> list[dict]:
     return make_query(query, amount)
 
 
-def search_by_username_with_content(username: str, content: str, amount: int = MAX_TWEETS) -> list[dict]:
+def search_by_username_with_content(username: str, content: str, amount: int = MAX_TWEETS):
     query = f'{content} from:{username} -filter:retweets'
     return make_query(query, amount)
 
 
-def search_by_hashtag():
-    pass
+def search_by_hashtag(hashtag: str, amount: int):
+    query = f'from:{hashtag} - filter:retweets'
+    """
+    found_tweets = api.search_tweets(q = hashtag, tweet_mode = 'extended', count = amount)
+    for tweet in found_tweets:
+        printTweet(tweet)
+    return listify_tweets(found_tweets)
+    """
+    return make_query(query, amount)
+
+
+def search_votes(contest):
+    """
+    Raccoglie tutti i voti del contest di id ''contest'' e li ritorna sotto forma di stringa
+
+    in seconda battuta va aggiunto il controllo che per ogni nome utente vanno presi solo gli ultimi 10 tweet che ha fatto
+    """
+    d = search_by_content(f'#IngSof2021vote #{contest}')
+    return d
+
+
+def get_coordinates(place: str):
+
+    url = "https://nominatim.openstreetmap.org/search?q=" + place + "&format=geojson"
+    payload = {}
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': 'youremail@domain.com'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    tmp = response.json()
+    print(tmp['features'][0]['geometry']['coordinates'])
+    return(tmp['features'][0]['geometry']['coordinates'])
+
+
+def search_by_geotag(place: str, amount: int):
+    """"
+    la query in input deve essere del tipo "lat, long, rad", es "40.730610, -73.935242, 1mi"
+    "40.7127281, -74.0060152, 1mi"
+    """
+    coordinates = get_coordinates(place)
+    long = coordinates[0]
+    lat = coordinates[1]
+    coordinate = str(lat) + "," + str(long) + "," + "6mi"
+    coordinate_tweet = str(lat) + "," + str(long)
+    print(coordinate)
+    tmp = coordinate
+
+    query = f'-filter:retweets'
+    found_tweets = api.search_tweets(
+        query, geocode=tmp, tweet_mode='extended', count=amount)
+
+    """print("ao")
+    for i in found_tweets:
+        printTweet(i)"""
+
+    return listify_tweets(found_tweets, coordinate_tweet)
